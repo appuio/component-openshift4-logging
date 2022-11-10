6a5a870be178b2d686c27a9acbb1cc7b01c456e8
Simon Gerber <simon.gerber@vshn.ch>
2022-11-10 11:22:41 +0100

    Add explicit test cases for cluster-logging `release-5.4` and `release-5.5`
    Rendered from template version: main (86f5428)

--- Makefile.vars.mk
+++ Makefile.vars.mk
@@ -57,4 +57,4 @@ KUBENT_IMAGE    ?= ghcr.io/doitintl/kube-no-trouble:latest
 KUBENT_DOCKER   ?= $(DOCKER_CMD) $(DOCKER_ARGS) $(root_volume) --entrypoint=/app/kubent $(KUBENT_IMAGE)
 
 instance ?= defaults
-test_instances = tests/defaults.yml tests/syn-monitoring.yml
+test_instances = tests/defaults.yml tests/syn-monitoring.yml tests/release-5.4.yml tests/release-5.5.yml
